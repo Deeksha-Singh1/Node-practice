46ff120312d33712ea33dc8d5e25ec05a352a93c
Deeksha-Singh1 <deeksha.singh1_cs19@gla.ac.in>
2023-05-11 15:13:19 +0530

    node.js practice

--- WebServerNode.js/index.js
+++ WebServerNode.js/index.js
@@ -1,6 +1,15 @@
 const http = require('http');
 const server=http.createServer((req,res)=>{
+if(req.url == '/')
   res.end('Hi all, Welcome to my page');
+else if(req.url=='/about')
+  res.end('Hi all, Welcome to my about us page');
+else if(req.url=='/contact')
+  res.end('Hi all, Welcome to my contact page'); 
+else{ 
+  res.writeHead(404,{"Content-type":"text/html"});
+  res.end('<h1>404 error Page does not exist</h1>');
+  }
 });
 
 server.listen(8000,'127.0.0.1',()=>{
