eb64ac80f1251522026fe971d5a663d7e8f1dc6b
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-04 23:50:19 +0530

    mongoose

--- mongooseP/src/app.js
+++ mongooseP/src/app.js
@@ -30,7 +30,7 @@ const playlistSchema = new mongoose.Schema({
 //collection creation
 const Playlist = new mongoose.model("Playlist",playlistSchema);
 
-//inserting data or creating a document
+/*inserting data or creating a document
 const createDoc = async ()=>{
 
   try{
@@ -72,14 +72,73 @@ const createDoc = async ()=>{
   catch(error){
     console.log(error);
   }
-}
+}*/
 
-//createDoc();
+/*gets the name field only where cType is Front End
 
 const getDoc = async ()=>{
-   const result = await Playlist.find({cType:"Front End"}).select({name:1, _id:0});
+   const result = await Playlist
+   .find({cType:"Front End"})
+   .select({name:1, _id:0});
    console.log(result);
 }
+*/
 
-getDoc();
+/* get the videos where videos are more than 50 
 
+const getDoc = async()=>{
+  const result = await Playlist
+  .find({videos:{$gt:50}}
+    )
+    console.log(result);
+}*/
+
+/* return if backend  and database present aas cType field
+
+const getDoc = async ()=>{
+  const result = await Playlist
+  .find({cType: { $in : ["Back End","Database"]}})
+  console.log(result);
+}*/
+
+/*logical queries
+
+const getDoc = async ()=>{
+  const result = await Playlist
+  .find({ $and : [{cType:"Back End"},
+{author:"xyz"}]})
+  console.log(result);
+}*/
+
+/*gets the count of all the record with author xyz
+
+const getDoc = async ()=>{
+  try{const result = await Playlist
+    .find({author:"xyz"})
+   .countDocuments();
+    console.log(result);
+  }catch(error){console.log(error);}}*/
+  
+//   const getDoc = async ()=>{
+//     try{
+//       const result = await Playlist
+//       .find({author:"xyz"})
+//       .select({name:1,_id:0})
+//      .sort({name:-1});
+//       console.log(result);
+//     }catch(error){console.log(error);}}
+
+//  getDoc();
+
+
+//delete the document
+
+const deleteDoc = async (_id)=>{
+  try{ const result = await Playlist
+    .deleteOne({_id});
+  console.log(result);}catch(error){console.log(error);}
+ 
+
+}
+
+deleteDoc("647ca67cfff367a85be5e32a");
