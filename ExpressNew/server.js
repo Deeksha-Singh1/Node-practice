22556f8fdc11a303287a7053fb01cde92d16c5ce
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-20 13:59:26 +0530

    ejs template engine

--- ExpressNew/server.js
+++ ExpressNew/server.js
@@ -4,17 +4,28 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-  res.sendFile(path.resolve(__dirname)+'/index.html');
-});
+app.set('view engine','ejs');
 
-app.get('/about',(eq, res)=>{
-  res.sendFile(path.resolve(__dirname)+'/about.html');
-});
+// app.get('/',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname)+'/index.html');
+// });
+
+// app.get('/about',(eq, res)=>{
+//   res.sendFile(path.resolve(__dirname)+'/about.html');
+// });
 
 //downloads the static file
 app.get('/download',(req,res)=>{
   res.download(path.resolve(__dirname)+'/about.html');
 })
+
+//with ejs template engine
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.get('/about',(eq, res)=>{
+  res.render('about')
+});
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`))
